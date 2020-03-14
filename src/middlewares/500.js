29590dcf779641ebe7870d92f448fb13b58fb320
Mohammed Atef <mohamed.atef@safedrivingnetwork.com>
2020-03-14 21:05:01 +0200

    changing project name

--- src/middlewares/500.js
+++ src/middlewares/500.js
@@ -1,5 +1,6 @@
 
 const logger = require('../utils/logger.js')
+
 module.exports = (err, req, res, next) => {
     if (!err) return next()
     logger.error(err.message, err)
--- src/startup/config.js
+++ src/startup/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    port: process.env.PORT,
-    db_URI: process.env.db_URI,
-    session_secret: process.env.session_secret,
-    paypal_mode: process.env.paypal_mode,
-    paypal_client_id: process.env.paypal_client_id,
-    paypal_client_secret: process.env.paypal_client_secret
-}
+  port: process.env.PORT,
+  db_URI: process.env.db_URI,
+  session_secret: process.env.session_secret,
+  paypal_mode: process.env.paypal_mode,
+  paypal_client_id: process.env.paypal_client_id,
+  paypal_client_secret: process.env.paypal_client_secret
+};
